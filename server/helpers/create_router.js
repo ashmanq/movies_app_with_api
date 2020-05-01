02e655e0b69a4d9354659ccdbb35ad5a1995b853
Ashir <ashirq@outlook.com>
2020-05-01 13:55:05 +0100

    App finds individual movie

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -8,8 +8,27 @@ const createRouter = function(collection) {
   router.get('/', function(req, res) {
     collection.find().toArray()
     .then(docs => res.json(docs))
+    .catch((error) => {
+      errorFunction(error, res);
+    })
   });
+
+  router.get('/:id', function(req, res) {
+    const id = req.params.id;
+    collection.findOne({_id: ObjectID(id)})
+    .then(doc => res.json(doc))
+    .catch((error) => {
+      errorFunction(error, res);
+    })
+  });
+  
   return router;
+};
+
+const errorFunction = function(error, res) {
+  console.error(error);
+  res.status(500);
+  res.json({error: error});
 };
 
 module.exports = createRouter;
